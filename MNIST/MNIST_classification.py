a6cc83bcfff9668c2d2a98ceb49c2088575f42c0
Feng Gao <gaofeng2@msu.edu>
2019-12-09 23:30:27 -0500

    Update MNIST_classification.py

--- MNIST/MNIST_classification.py
+++ MNIST/MNIST_classification.py
@@ -20,30 +20,33 @@ test_indices = [i for i, x in enumerate(test_Y) if x == 6]
 
 
 
-training_signal = np.load('train_feature0.npy')
+training_signal = np.load('train_featureJ0.npy')
 
-test_signal = np.load('test_feature0.npy')
+test_signal = np.load('test_featureJ0.npy')
 
 
 training_Y_ = [x for i, x in enumerate(training_Y) if i not in train_indices]
 test_Y_ = [x for i, x in enumerate(test_Y) if i not in test_indices]
 
+#as an example
 G_pool = [0.04]
 C_pool = [650]
 
 training_feature = np.reshape(training_signal,(len(training_signal),training_signal[0].shape[0]))
 training_feature = training_feature[:,selected_feature]
-#np.save('train_downsample',training_feature)
+#the first time save downsampling features
+np.save('train_downsample',training_feature)
+#once saved these can be used in the future
 #training_feature = np.load('train_downsample.npy')
 #training_feature_z = scipy.stats.mstats.zscore(training_feature,0)
 
 
 
-#test_feature = np.reshape(test_signal,(len(test_signal),test_signal[0].shape[0]))
-#test_feature = test_feature[:,selected_feature]
-#np.save('test_downsample',test_feature)
-test_feature = np.load('test_downsample.npy')
-test_feature_z = scipy.stats.mstats.zscore(test_feature,0)
+test_feature = np.reshape(test_signal,(len(test_signal),test_signal[0].shape[0]))
+test_feature = test_feature[:,selected_feature]
+np.save('test_downsample',test_feature)
+#test_feature = np.load('test_downsample.npy')
+#test_feature_z = scipy.stats.mstats.zscore(test_feature,0)
 print('begin cross validation')
 
 result,prediction_acc = cross_validate(5,training_feature_z,training_Y_,test_feature_z,test_Y_,G_pool,C_pool)
